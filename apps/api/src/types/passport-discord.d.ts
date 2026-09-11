27cdd8d32b8b564ec7314019184e9524bd96000b
Naveen <naveenk4250@gmail.com>
2026-09-11 17:00:22 +0530

    chore(api): remove unused passport-discord dependency and type declaration

--- apps/api/src/types/passport-discord.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'passport-discord' {
-  import { Strategy as PassportStrategy } from 'passport';
-  export class Strategy extends PassportStrategy {
-    constructor(options: any, verify: any);
-  }
-}
